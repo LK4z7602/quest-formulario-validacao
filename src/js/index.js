43b78108ddb9af5d05f8eaa6022624629e135a64
LK4z <lk4zvpn@gmail.com>
2025-07-29 18:09:44 -0300

    init

---
+++ src/js/index.js
@@ -0,0 +1,22 @@
+const inputsTexto = document.querySelectorAll(".input");
+const spans = document.querySelectorAll("span");
+const itens = document.querySelectorAll(".item");
+const botao = document.getElementById("enviar");
+
+botao.addEventListener("click", function () {
+  inputsTexto.forEach((input, idx) => {
+    const temTexto = input.value.trim() !== "";
+
+    if (temTexto) {
+      input.classList.remove("incorreto");
+      input.classList.add("correto");
+      spans[idx].classList.remove("ativo");
+      itens[idx].classList.remove("span-ativo");
+    } else {
+      input.classList.remove("correto");
+      input.classList.add("incorreto");
+      spans[idx].classList.add("ativo");
+      itens[idx].classList.add("span-ativo");
+    }
+  });
+});
